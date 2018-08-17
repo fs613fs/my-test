2215fbe8bbf27a205316c9900084f1a41ac7482c
Fei Sun <Fei.Sun@opencolleges.edu.au>
2018-08-17 15:22:45 +1000

    deployment script

---
+++ upload-portfolio.py
@@ -0,0 +1,19 @@
+import boto3
+from botocore.client import Config
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+
+build_bucket = s3.Bucket('portfoliobuild.feisun.me')
+portfolio_bucket = s3.Bucket('feisun.me')
+
+# On Windows, this will need to be a different location than /tmp
+build_bucket.download_file('buildfeisun.zip', '/tmp/portfolio.zip')
+
+with zipfile.ZipFile('/tmp/portfolio.zip') as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm,
+          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
