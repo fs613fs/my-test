63fe0c0d20adc7b8159197a99b6bfecd224855da
Fei Sun <Fei.Sun@opencolleges.edu.au>
2018-08-17 17:22:45 +1000

    updated with the working version

--- upload-portfolio.py
+++ upload-portfolio.py
@@ -7,14 +7,27 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:930720100739:deployPortfolioTopic')
 
+    location = {
+        "bucketName" : "portfoliobuild.feisun.me",
+        "objectKey": 'buildfeisun.zip'
+    }
+
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3location"]
+        print "Building portfolio from " + str(location)
+
         s3 = boto3.resource('s3')
 
-        build_bucket = s3.Bucket('portfoliobuild.feisun.me')
         portfolio_bucket = s3.Bucket('feisun.me')
+        build_bucket = s3.Bucket(location['bucketName'])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('buildfeisun.zip', portfolio_zip)
+        build_bucket.download_fileobj(location['objectKey'], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -23,6 +36,9 @@ def lambda_handler(event, context):
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
         print 'Job done'
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed Successfully")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except Exception, e:
-        topic.publish(Message="Portfolio Deployment Failed",Subject="Portfolio Deployment Failed")
+        topic.publish(Subject="Portfolio Deployment Failed", Message="Portfolio Deployment Failed")
         raise e
